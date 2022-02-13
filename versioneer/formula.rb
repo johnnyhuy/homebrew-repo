f385d8f8bb9ef44a94601f81185503b2e6d61ea7
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2022-02-13 18:05:27 +1100

    wip: Hardware check

--- versioneer/formula.rb
+++ versioneer/formula.rb
@@ -7,14 +7,17 @@ class Versioneer < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/johnnyhuy/versioneer/releases/download/v#{version}/versioneer-macos-arm64.tar.gz"
-    elsif
+    elsif Hardware::CPU.is_64_bit?
       url "https://github.com/johnnyhuy/versioneer/releases/download/v#{version}/versioneer-macos-x64.tar.gz"
     end
   elsif OS.linux?
-    url "https://github.com/johnnyhuy/versioneer/releases/download/v#{version}/versioneer-linux-macos-x64.tar.gz"
+    url "https://github.com/johnnyhuy/versioneer/releases/download/v#{version}/versioneer-linux-x64.tar.gz"
   elsif OS.windows?
     url "https://github.com/johnnyhuy/versioneer/releases/download/v#{version}/versioneer-win-macos-x64.tar.gz"
   end
+
+  depends_on "git"
+  depends_on "zsh" => :optional
 
   def install
     bin.install "versioneer"
